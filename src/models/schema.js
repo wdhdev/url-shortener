865cc23fd4a0289362cce0d381b2f54105a77578
William Harrison <william@williamdavidharrison.com.au>
2023-03-12 13:07:27 +0800

    Delete src directory

--- src/models/schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-    path: String,
-    redirect: String,
-    redirect_path: Boolean
-})
-
-module.exports = mongoose.model("redirects", schema, "redirects")
