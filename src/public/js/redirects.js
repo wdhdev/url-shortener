803c4026e80c596124f3f70064d32e3451c83025
William Harrison <william@williamdavidharrison.com.au>
2023-05-08 14:02:56 +0800

    feat: update redirect path col + remove sorting

--- src/public/js/redirects.js
+++ src/public/js/redirects.js
@@ -1,4 +1,3 @@
-const table = document.getElementById("redirects");
 const tableBody = document.getElementById("redirects-body");
 
 function loadRedirects() {
@@ -16,49 +15,16 @@ function loadRedirects() {
 
             c1.classList = "px-4 py-2 outline outline-1 outline-gray-700";
             c2.classList = "px-4 py-2 outline outline-1 outline-gray-700";
-            c3.classList = "px-4 py-2 outline outline-1 outline-gray-700";
+            c3.classList = "px-4 py-2 outline outline-1 outline-gray-700 text-center";
 
-            const redirect_path = {
-                false: "No",
-                true: "Yes"
-            }
-
-            const redirect_path_colour = {
-                false: "text-red-600",
-                true: "text-green-600"
+            const icon = {
+                false: "fa-solid fa-x text-red-600",
+                true: "fa-solid fa-check text-green-600"
             }
 
             c1.innerHTML = `<a href="/${redirect.path}" class="font-semibold text-blue-600 hover:text-blue-700">/${redirect.path}</a>`;
-            c1.setAttribute("data-column", "path");
-
             c2.innerHTML = `<a href="${redirect.redirect}" class="text-gray-400 underline underline-2 hover:no-underline">${redirect.redirect}</a>`;
-            c2.setAttribute("data-column", "redirect");
-
-            c3.innerHTML = `<span class="${redirect_path_colour[redirect.redirect_path]}">${redirect_path[redirect.redirect_path]}</span>`;
-            c3.setAttribute("data-column", "redirect-path");
-        })
-    })
-}
-
-function sortTable() {
-    const headers = table.querySelectorAll("th[data-sort]");
-    const rows = Array.from(table.querySelectorAll("tbody tr"));
-
-    headers.forEach((header) => {
-        header.addEventListener("click", () => {
-            const column = header.getAttribute("data-sort");
-            const direction = header.classList.contains("asc") ? -1 : 1;
-
-            rows.sort((a, b) => {
-                const aValue = a.querySelector(`td[data-column="${column}"]`).textContent;
-                const bValue = b.querySelector(`td[data-column="${column}"]`).textContent;
-                return direction * aValue.localeCompare(bValue);
-            });
-
-            rows.forEach((row) => table.tBodies[0].appendChild(row));
-
-            headers.forEach((h) => h.classList.remove("asc", "desc"));
-            header.classList.toggle(direction === 1 ? "asc" : "desc");
+            c3.innerHTML = `<i class="${icon[redirect.redirect_path]}"></i>`;
         })
     })
 }
