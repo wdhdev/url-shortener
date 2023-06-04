a9246c5373d8ac990ce98cc0e8f6476a3ef1b45f
William Harrison <william@williamdavidharrison.com.au>
2023-06-04 13:33:31 +0800

    feat: move to root dir + add sentry

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+
+const Sentry = require("@sentry/node");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-require("dotenv").config();
+Sentry.init({
+    dsn: process.env.sentry_dsn,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Sentry.Integrations.Express({ app }),
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+    ],
+    tracesSampleRate: 1.0
+})
 
 const port = process.env.port || 3000;
 const router = require("./util/router");
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,6 +38,8 @@ database();
 
 // Router
 app.use("/", router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => {
     console.log(`Listening on Port: ${port}`);
