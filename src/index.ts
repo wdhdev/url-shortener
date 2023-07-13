196f8efa3d93bb2bb0bcd4101d66dc6884b64c56
William Harrison <william@williamdavidharrison.com.au>
2023-07-13 13:50:40 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.set("view engine", "ejs");
 app.set("views", "src/views");
 
 // Host public files
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "src/public"));
 
 // Connect to Database
 const database = require("./util/database");
